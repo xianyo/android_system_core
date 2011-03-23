081856235f86701e3310118885a4b321cfa4001a
guoyin.chen <guoyin.chen@freescale.com>
2011-03-23 18:45:52 -0400

    ENGR00141046 Integrate xec into android
    Change xec permission group to SYSTEM
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- init/property_service.c
+++ init/property_service.c
@@ -82,7 +82,7 @@ struct {
     { "persist.sys.",     AID_SYSTEM,   0 },
     { "persist.service.", AID_SYSTEM,   0 },
     { "persist.security.", AID_SYSTEM,  0 },
-    { "xec.",             AID_MEDIA,    0 },
+    { "xec.",             AID_SYSTEM,    0 },
     { NULL, 0, 0 }
 };
 
