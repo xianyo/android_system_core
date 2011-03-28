8cc95d7d0311f0e80003d3b7604332fcf28e7752
Zhang Jiejing <jiejing.zhang@freescale.com>
2011-03-28 17:13:25 +0800

    ENGR00141264 fix recovery image permission issue
    make_ext4fs.c will read permissions define in this header file,
    since it's leak of recovery related define, so it will lost
    all execute permission.
    
    Signed-off-by: Zhang Jiejing <jiejing.zhang@freescale.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -174,6 +174,9 @@ static struct fs_path_config android_files[] = {
     { 00640, AID_SYSTEM,    AID_SYSTEM,    "system/etc/bluetooth/auto_pairing.conf" },
     { 00444, AID_RADIO,     AID_AUDIO,     "system/etc/AudioPara4.csv" },
     { 00555, AID_ROOT,      AID_ROOT,      "system/etc/ppp/*" },
+    { 00750, AID_ROOT,      AID_SHELL,     "recovery/init*" },
+    { 00755, AID_ROOT,      AID_SHELL,     "recovery/system/bin/*" },
+    { 00755, AID_ROOT,      AID_SHELL,     "recovery/sbin/*" },
     { 00644, AID_SYSTEM,    AID_SYSTEM,    "data/app/*" },
     { 00644, AID_SYSTEM,    AID_SYSTEM,    "data/app-private/*" },
     { 00644, AID_APP,       AID_APP,       "data/data/*" },
@@ -228,8 +231,8 @@ static inline void fs_config(const char *path, int dir,
     *mode = (*mode & (~07777)) | pc->mode;
     
 #if 0
-    fprintf(stderr,"< '%s' '%s' %d %d %o >\n", 
-            path, pc->prefix ? pc->prefix : "", *uid, *gid, *mode);
+    printf("< '%s' '%s' %d %d %o >\n", path,
+	   pc->prefix ? pc->prefix : "", *uid, *gid, *mode);
 #endif
 }
 #endif
