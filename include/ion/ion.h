de46f91cc67f2987cb3a68597ba7458caf2d54c3
Xinyu Chen <xinyu.chen@freescale.com>
2012-12-13 17:59:56 +0800

    ENGR00236857: revert libion back to jb4.1.x version
    The libion in JB4.2 has updated to use 3.4 kernel driver.
    But in our kernel 3.0.35, the DMA_SHARED_BUFFER
    feature is not supported, so new ion cannot be used.
    
    Change-Id: Ib2cad9712f435bacdcdc213d72e7efd02e084140
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- include/ion/ion.h
+++ include/ion/ion.h
@@ -27,16 +27,14 @@ __BEGIN_DECLS
 
 int ion_open();
 int ion_close(int fd);
-int ion_alloc(int fd, size_t len, size_t align, unsigned int heap_mask,
-	      unsigned int flags, struct ion_handle **handle);
-int ion_alloc_fd(int fd, size_t len, size_t align, unsigned int heap_mask,
-		 unsigned int flags, int *handle_fd);
-int ion_sync_fd(int fd, int handle_fd);
+int ion_alloc(int fd, size_t len, size_t align, unsigned int flags,
+              struct ion_handle **handle);
 int ion_free(int fd, struct ion_handle *handle);
 int ion_map(int fd, struct ion_handle *handle, size_t length, int prot,
             int flags, off_t offset, unsigned char **ptr, int *map_fd);
 int ion_share(int fd, struct ion_handle *handle, int *share_fd);
 int ion_import(int fd, int share_fd, struct ion_handle **handle);
+unsigned long ion_phys(int fd, struct ion_handle *handle);
 
 __END_DECLS
 
