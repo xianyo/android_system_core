0861c79a15a0f20e9bd89b31528ccafa1ca0dbe2
guoyin.chen <guoyin.chen@freescale.com>
2013-11-27 13:45:16 +0800

    ENGR00289825 Make simulated battery status report based on sys.emulated.battery
    Fake a battery status based on sys.emulated.battery property setting
    Boards without battery support should have this property set, due to some
    function like data encryption need to check battery status.
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (C) 2013 Freescale Semiconductor, Inc.
+ */
+
 #define LOG_TAG "healthd"
 
 #include "healthd.h"
@@ -313,6 +317,15 @@ bool BatteryMonitor::update(void) {
         }
 
         KLOG_WARNING(LOG_TAG, "%s\n", dmesgline);
+    }
+
+    char prop[PROP_VALUE_MAX];
+    // always report AC plug-in and capacity 100% if emulated.battery is set to 1
+    property_get("sys.emulated.battery", prop, "0");
+    if (!strcmp(prop, "1")){
+        props.chargerAcOnline = true;
+        props.batteryLevel = 100;
+        props.chargerWirelessOnline = false;
     }
 
     healthd_mode_ops->battery_update(&props);
