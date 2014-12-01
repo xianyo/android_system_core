61e01da36d249adda06b6a8794b39805fe496d20
zhang sanshan <b51434@freescale.com>
2014-12-01 15:09:35 +0800

    MA-5911 change selinux policy
    allow disable selinux in user mode
    
    Signed-off-by: zhang sanshan <b51434@freescale.com>

--- init/Android.mk
+++ init/Android.mk
@@ -24,9 +24,7 @@ LOCAL_SRC_FILES += bootchart.c
 LOCAL_CFLAGS    += -DBOOTCHART=1
 endif
 
-ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DALLOW_LOCAL_PROP_OVERRIDE=1 -DALLOW_DISABLE_SELINUX=1
-endif
 
 # Enable ueventd logging
 #LOCAL_CFLAGS += -DLOG_UEVENTS=1
